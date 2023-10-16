3da02399c6ddd7879519234772b7d04a25204432
shoshy4 <s0583210627@gmail.com>
2023-10-16 15:04:33 +0300

    changing more views from function-based to class-based

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.views import View
 from django.views.generic import ListView, DetailView
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
@@ -20,6 +22,8 @@ class PostDetailView(DetailView):
     def get_object(self):
         obj = super().get_object()
         return obj
+
+
 
 @login_required
 def post_new(request):
@@ -43,17 +47,17 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
-@login_required
-def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
-    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+class PostDraftListView(ListView):
+    model = Post
+    context_object_name = "posts"
+    queryset = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+    template_name = 'blog/post_draft_list.html'
 
 @login_required
 def post_publish(request, pk):
@@ -66,18 +70,34 @@ def post_remove(request, pk):
     post.delete()
     return redirect('post_list')
 
-def add_comment_to_post(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = CommentForm(request.POST)
+class CommentFormView(View):
+    form_class = CommentForm
+    initial = {"key": "value"}
+    template_name = "blog/add_comment_to_post.html"
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.post = post
+            comment.post = get_object_or_404(Post, pk=self.kwargs["pk"])
             comment.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = CommentForm()
-    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+            return redirect('post_detail', pk=self.kwargs["pk"])
+        return render(request, self.template_name, {"form": form})
+#def add_comment_to_post(request, pk):
+ #   post = get_object_or_404(Post, pk=pk)
+  #  if request.method == "POST":
+   ##    if form.is_valid():
+     #       comment = form.save(commit=False)
+      #      comment.post = post
+       #     comment.save()
+        #    return redirect('post_detail', pk=post.pk)
+    #else:
+     #   form = CommentForm()
+    #return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
 @login_required
 def comment_approve(request, pk):
